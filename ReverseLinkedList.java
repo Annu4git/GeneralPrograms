46f6b58f4fbc60727a4c4aec5186359366f362e3
Ashish Chaturvedi <36396221+aashiishh@users.noreply.github.com>
2020-10-26 17:20:05 +0530

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class ReverseLinkedList
+{
+	public static void main(String args[])
+	{
+        LinkedList list = new LinkedList();
+        list.add(1);
+        System.out.println(list.get(0));
+	}
+}
